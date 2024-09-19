8dd0041f90879e548fb40a069ccdc18d14e0da90
hoangphucquan2004 <quan180604@gmail.com>
2024-09-19 23:22:44 +0700

    donhang

--- resources/views/admin/don-hang/index.blade.php
+++ resources/views/admin/don-hang/index.blade.php
@@ -177,19 +177,21 @@
         document.getElementById("table-gridjs") && new gridjs.Grid({
             columns: [{
                     name: "STT",
-                    width: "80px",
+                    width: "8%",
                     formatter: function(e) {
+                        const id = e;
+                        const detailUrl = "{{ route('don-hang.detail', ':id') }}".replace(':id', id);
                         return gridjs.html(`
                         <div class="flex-grow-1">
                             <span class="fw-semibold">  ${e}</span>
                         </div>
                          <div class="d-flex justify-content-start mt-2">
-                        <a href="{{ route('banner.detail') }}" class="btn btn-link p-0">Xem</a>
+                        <a href="${detailUrl}" class="btn btn-link p-0">Xem</a>
                     </div>`)
                     }
                 }, {
                     name: "Họ và tên",
-                    width: "150px",
+                    width: "15%",
                     formatter: function(e) {
                         return gridjs.html(` ${e}
                    
@@ -198,28 +200,35 @@
                 },
                 {
                     name: "Email",
-                    width: "180px",
+                    width: "20%",
                     formatter: function(e) {
                         return gridjs.html('<a href="">' + e + "</a>")
                     }
                 },
                 {
                     name: "Sách",
-                    width: "180px",
+                    width: "20%",
+                    formatter: function(e) {
+                        return gridjs.html('<a href="">' + e + "</a>")
+                    }
+                },
+                {
+                    name: "Số tiền",
+                    width: "10%",
                     formatter: function(e) {
                         return gridjs.html('<a href="">' + e + "</a>")
                     }
                 },
                 {
                     name: "Phương thức",
-                    width: "150px",
+                    width: "15%",
                     formatter: function(e) {
                         return gridjs.html('<a href="">' + e + "</a>")
                     }
                 },
                 {
                     name: "Trạng thái",
-                    width: "150px",
+                    width: "12%",
                     formatter: function(e) {
                         let colorClass = '';
                         let xuLy = '';
@@ -258,6 +267,7 @@
                         '{{ $donHang->user->ten_doc_gia }}',
                         '{{ $donHang->user->email }}',
                         '{{ $donHang->sach->ten_sach }}',
+                        '{{ $donHang->so_tien_thanh_toan }}',
                         '{{ $donHang->phuongThucThanhToan->ten_phuong_thuc }}',
                         '{{ $donHang->trang_thai }}',
                     ],
