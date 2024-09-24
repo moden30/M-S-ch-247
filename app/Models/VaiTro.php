85b16b8b2d749a2dbc4ec19881cabf72bcf2e557
NguyenManhTuan04 <nguyemanhtuan040204@gmail.com>
2024-09-24 15:38:52 +0700

    Chuyển nhánh

--- app/Models/VaiTro.php
+++ app/Models/VaiTro.php
@@ -11,17 +11,17 @@ class VaiTro extends Model
 
     protected $table = 'vai_tros';
     protected $fillable = [
-        'ten_vai_tro', 
-        'mo_ta', 
+        'ten_vai_tro',
+        'mo_ta',
         'trang_thai',
-    ]
+    ];
 
     // Mối quan hệ nhiều-nhiều giữa VaiTro và Quyen
     public function quyens()
     {
         return $this->belongsToMany(Quyen::class, 'quyen_vai_tros', 'vai_tro_id', 'quyen_id');
     }
-    
 
-    
+
+
 }
