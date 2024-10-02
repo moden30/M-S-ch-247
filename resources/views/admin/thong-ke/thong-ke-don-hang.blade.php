93520390f7f9e0f6f350d27f6d8d2f2e8f923f82
quangson23 <nguyenquangson23022004@gmail.com>
2024-10-03 00:03:30 +0700

    thongkedonhang4

--- resources/views/admin/thong-ke/thong-ke-don-hang.blade.php
+++ resources/views/admin/thong-ke/thong-ke-don-hang.blade.php
@@ -112,3 +112,49 @@
                 </div><!-- end card body -->
             </div><!-- end card -->
         </div><!-- end col -->
+        <div class="col-xl-3 col-md-6">
+            <!-- card -->
+            <div class="card card-animate">
+                <div class="card-body">
+                    <div class="d-flex align-items-center">
+                        <div class="flex-grow-1">
+                            <p class="text-uppercase fw-medium text-muted mb-0">ĐƠN CHƯA THANH TOÁN</p>
+                        </div>
+                        <div class="flex-shrink-0">
+                            <h5
+                                class="{{ $hoaDonHomNay < $hoaDonHomQua ? 'text-danger' : 'text-success' }} fs-14 mb-0">
+                                <i
+                                    class="{{ $hoaDonHomNay < $hoaDonHomQua ? 'ri-arrow-right-down-line' : 'ri-arrow-right-up-line' }} fs-13 align-middle"></i>
+                                @if ($hoaDonHomQua > 0)
+                                    {{ $hoaDonHomNay < $hoaDonHomQua ? '-' : '+' }}
+                                    {{ number_format(abs((($hoaDonHomNay - $hoaDonHomQua) / $hoaDonHomQua) * 100), 2) }}
+                                    %
+                                @else
+                                    {{-- Nếu không có hóa đơn hôm qua và có hóa đơn hôm nay thì thay đổi là 100% --}}
+                                    @if ($hoaDonHomNay > 0)
+                                        + 100 %
+                                    @else
+                                        0 %
+                                    @endif
+                                @endif
+                            </h5>
+                        </div>
+
+                    </div>
+                    <div class="d-flex align-items-end justify-content-between mt-4">
+                        <div>
+                            <h4 class="fs-22 fw-semibold ff-secondary mb-4">
+                                <span class="counter-value" data-target="{{ $hoaDonHomNay }}"></span> Đơn hàng
+                            </h4>
+                            <span class="badge bg-warning me-1">{{ $hoaDonHomNay }}</span>
+                            <span class="text-muted">Đơn chưa thanh toán bởi khách hàng</span>
+                        </div>
+                        <div class="avatar-sm flex-shrink-0">
+                            <span class="avatar-title bg-light rounded fs-3">
+                                <i data-feather="clock" class="text-success icon-dual-success"></i>
+                            </span>
+                        </div>
+                    </div>
+                </div><!-- end card body -->
+            </div><!-- end card -->
+        </div><!-- end col -->
