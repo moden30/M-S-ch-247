2047077f6522e7445755d8a2930d0db24805457f
NguyenManhTuan04 <nguyemanhtuan040204@gmail.com>
2024-10-09 18:51:38 +0700

    client

--- resources/views/client/layouts/app.blade.php
+++ resources/views/client/layouts/app.blade.php
@@ -1,49 +1,61 @@
-<footer class="my-6 bg-background pb-6 text-white">
-    <figure class="relative flex min-h-[150px] w-full items-center justify-center">
-        <img class="h-[40px] w-[130px] fill-white md:h-[50px] md:w-[200px]" src="{{ asset('assets/client/images/logo/logo.png')}}" alt="logo">
-    </figure>
-    <div class="absolute-center mb-4 w-full space-x-6">
-        <button class="transition-all duration-200 hover:text-primary">
-            <a href="index.html" target="_blank" rel="noreferrer">
-                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 448 512" class="h-8 w-8"
-                    height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z">
-                    </path>
-                </svg>
-            </a>
-        </button>
-        <button class="transition-all duration-200 hover:text-primary">
-            <a href="index.html" target="_blank" rel="noreferrer">
-                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 640 512" class="h-8 w-8"
-                    height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M524.531,69.836a1.5,1.5,0,0,0-.764-.7A485.065,485.065,0,0,0,404.081,32.03a1.816,1.816,0,0,0-1.923.91,337.461,337.461,0,0,0-14.9,30.6,447.848,447.848,0,0,0-134.426,0,309.541,309.541,0,0,0-15.135-30.6,1.89,1.89,0,0,0-1.924-.91A483.689,483.689,0,0,0,116.085,69.137a1.712,1.712,0,0,0-.788.676C39.068,183.651,18.186,294.69,28.43,404.354a2.016,2.016,0,0,0,.765,1.375A487.666,487.666,0,0,0,176.02,479.918a1.9,1.9,0,0,0,2.063-.676A348.2,348.2,0,0,0,208.12,430.4a1.86,1.86,0,0,0-1.019-2.588,321.173,321.173,0,0,1-45.868-21.853,1.885,1.885,0,0,1-.185-3.126c3.082-2.309,6.166-4.711,9.109-7.137a1.819,1.819,0,0,1,1.9-.256c96.229,43.917,200.41,43.917,295.5,0a1.812,1.812,0,0,1,1.924.233c2.944,2.426,6.027,4.851,9.132,7.16a1.884,1.884,0,0,1-.162,3.126,301.407,301.407,0,0,1-45.89,21.83,1.875,1.875,0,0,0-1,2.611,391.055,391.055,0,0,0,30.014,48.815,1.864,1.864,0,0,0,2.063.7A486.048,486.048,0,0,0,610.7,405.729a1.882,1.882,0,0,0,.765-1.352C623.729,277.594,590.933,167.465,524.531,69.836ZM222.491,337.58c-28.972,0-52.844-26.587-52.844-59.239S193.056,219.1,222.491,219.1c29.665,0,53.306,26.82,52.843,59.239C275.334,310.993,251.924,337.58,222.491,337.58Zm195.38,0c-28.971,0-52.843-26.587-52.843-59.239S388.437,219.1,417.871,219.1c29.667,0,53.307,26.82,52.844,59.239C470.715,310.993,447.538,337.58,417.871,337.58Z">
-                    </path>
-                </svg>
-            </a>
-        </button>
-    </div>
-    <div class="mx-auto flex w-[70%] flex-col justify-center space-y-4 text-lg md:text-2xl">
-        <h1 class="text-center font-secondary text-2xl font-bold md:text-4xl">NQT Comics là website đọc truyện tranh
-            miễn phí</h1>
-        <div class="flex flex-col items-center justify-center gap-4 md:flex-row md:space-x-8 md:text-2xl">
-            <a class="hover:text-primary" href="dieu-khoan-dich-vu.html">Điều khoản dịch vụ</a>
-            <a class="hover:text-primary" href="chinh-sach-bao-mat.html">Chính sách riêng tư</a>
-            <a class="hover:text-primary" href="dmca.html">DMCA</a>
-            <a class="hover:text-primary" href="lien-he.html">Liên hệ</a>
-        </div>
-    </div>
-    <div class="my-4 mx-auto w-[70%] space-y-4 lg:w-1/2">
-        <p class="text-center text-lg text-white/40 md:text-2xl">NQT Comics không lưu trữ bất kì tệp tin nào trên
-            máy chủ, chúng tôi chỉ liên kết tới những phương tiện truyền thông được lưu trữ bên dịch vụ thứ 3.</p>
-        <p class="text-center text-lg text-white/40 md:text-2xl">NQT Comics does not store any files on our server,
-            we only linked to the media which is hosted on 3rd party services.</p>
-        <p class="text-center text-lg text-white/40 md:text-2xl">© NQT Comics</p>
-    </div>
-</footer>
+<!DOCTYPE html>
+<html lang="vi">
 
-<script src="../cdnjs.cloudflare.com/ajax/libs/lozad.js/1.0.8/lozad.min.js"></script>
+<!-- Mirrored from demo.nqtcomics.site/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 15 Sep 2024 09:29:00 GMT -->
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8"/><!-- /Added by HTTrack -->
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <meta name="csrf-token" content="dXAycXLI0OugQtFu7cyIcLma4Oe82GOj5qHdorRO">
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.png">
+    <link rel="preload" as="style" href="https://demo.nqtcomics.site/build/assets/app-B-gB5l2A.css"/>
+    <link rel="stylesheet" href="https://demo.nqtcomics.site/build/assets/app-B-gB5l2A.css"/>
+    <script src="https://demo.nqtcomics.site/assets/js/jquery.min.js"></script>
+    <script src="https://demo.nqtcomics.site/assets/js/main.js"></script>
+    <title>NQTComics đọc truyện miễn phí</title>
+    <meta name="description" content="NQTComics đọc truyện miễn phí">
+    <meta name="keywords" content="nqtcomcis">
+    <meta property="article:published_time" content="2024-10-09 17:52:46">
+    <link rel="canonical" href="https://demo.nqtcomics.site">
+    <meta property="og:title" content="NQTComics đọc truyện miễn phí">
+    <meta property="og:description" content="NQTComics đọc truyện miễn phí">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="/favicon.png">
+
+    <meta name="twitter:title" content="NQTComics đọc truyện miễn phí">
+    <meta name="twitter:description" content="NQTComics đọc truyện miễn phí">
+    <script type="application/ld+json">
+        {"@context":"https://schema.org","@type":"WebPage","name":"NQTComics đọc truyện miễn phí","description":"NQTComics đọc truyện miễn phí","url":"https://demo.nqtcomics.site"}
+    </script>
+    <meta name="robots" content="index,follow"/>
+    <meta name="revisit-after" content="1 days"/>
+    <meta name="ROBOTS" content="index,follow"/>
+    <meta name="googlebot" content="index,follow"/>
+    <meta name="BingBOT" content="index,follow"/>
+    <meta name="yahooBOT" content="index,follow"/>
+    <meta name="slurp" content="index,follow"/>
+    <meta name="msnbot" content="index,follow"/>
+    <link rel="stylesheet" href="https://demo.nqtcomics.site/assets/css/swiper-bundle.min.css"/>
+    <script src="https://demo.nqtcomics.site/assets/js/swiper-bundle.min.js"></script>
+    @stack('styles')
+</head>
+
+<body>
+@include('client.components.header')
+<!-- end header -->
+@include('client.components.sidebar-mobile')
+{{--end sidebar mobile--}}
+
+@yield('content')
+@include('client.components.footer')
+{{--end footer--}}
+<script src="{{ asset('assets/client/cdnjs.cloudflare.com/ajax/libs/lozad.js/1.0.8/lozad.min.js') }}"></script>
+@stack('scripts')
 <script>
     const observer = lozad();
     observer.observe();
@@ -122,7 +134,7 @@
         });
     });
 </script>
-<script src="{{ asset('assets/client/js/swiper.js')}}"></script>
+<script src="{{ asset('assets/client/js/swiper.js') }}"></script>
 <script>
     document.addEventListener('DOMContentLoaded', function () {
         const allTabs = document.querySelectorAll('.server-tab');
