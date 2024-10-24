edd1ffd34571fca449b38c7f515c55d97fb715e8
Moden <lann47897@gmail.com>
2024-10-24 16:30:53 +0700

    danh sách ctv lần 3

---
+++ resources/views/admin/kiem-duyet-cong-tac-vien/index.blade.php
@@ -0,0 +1,302 @@
+@extends('admin.layouts.app')
+@section('start-point')
+    Yêu cầu đăng ký cộng tác viên
+@endsection
+@section('title')
+    Danh sách
+@endsection
+@section('content')
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title mb-0 flex-grow-1">Danh sách yêu cầu đăng ký cộng tác viên</h4>
+
+                </div><!-- end card header -->
+
+                <div class="card-body">
+                    <div id="table-gridjs"></div>
+                </div><!-- end card-body -->
+            </div><!-- end card -->
+        </div>
+        <!--end col-->
+    </div>
+    <!--end row-->
+@endsection
+
+@push('styles')
+    <!-- Sweet Alert css-->
+    <!-- Sweet Alert css-->
+    <link href="{{ asset('assets/admin/libs/sweetalert2/sweetalert2.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- gridjs css -->
+    <link rel="stylesheet" href="{{ asset('assets/admin/libs/gridjs/theme/mermaid.min.css') }}">
+@endpush
+
+@push('scripts')
+    <!-- list.js min js -->
+    <script src="{{ asset('assets/admin/libs/list.js/list.min.js') }}"></script>
+
+    <!--list pagination js-->
+    <script src="{{ asset('assets/admin/libs/list.pagination.js/list.pagination.min.js') }}"></script>
+
+
+
+    <!-- Sweet Alerts js -->
+    <script src="{{ asset('assets/admin/libs/sweetalert2/sweetalert2.min.js') }}"></script>
+
+    <!-- prismjs plugin -->
+    <script src="{{ asset('assets/admin/libs/prismjs/prism.js') }}"></script>
+
+    <!-- gridjs js -->
+    <script src="{{ asset('assets/admin/libs/gridjs/gridjs.umd.js') }}"></script>
+    <!--  Đây là chỗ hiển thị dữ liệu phân trang -->
+
+    <script>
+        document.getElementById("table-gridjs") && new gridjs.Grid({
+            columns: [{
+                name: "STT",
+                hidden: true,
+            }, {
+                name: "Họ và tên",
+                width: "auto",
+                formatter: function(e, row) {
+                    const id = row.cells[0].data;
+                    {{--const detailUrl = "{{ route('yeu-cau-rut-tien.show', ':id') }}".replace(':id', id);--}}
+
+                        return gridjs.html(`
+                            <div class="flex-grow-1">
+                                <span class="fw-semibold">  ${e}</span>
+                            </div>
+                            <div class="d-flex justify-content-start mt-2">
+                                <a href="" class="btn btn-link p-0">Xem</a>
+                            </div>
+                        `);
+                }
+            },
+                {
+                    name: "Email",
+                    width: "auto",
+                    formatter: function(e) {
+                        return gridjs.html('<a >' + e + "</a>")
+                    }
+                },
+                {
+                    name: "Số điện thoại",
+                    width: "auto",
+                    formatter: function(e) {
+                        const formattedPhone = e.replace(/(\d{3})(\d{3})(\d{4})/, '$1 $2 $3');
+                        return gridjs.html('<a>' + formattedPhone + '</a>');
+                    }
+                },
+                {
+                    name: "Ngày tháng năm sinh",
+                    width: "auto",
+                    formatter: function(cell) {
+                        const date = new Date(cell);
+                        const formattedDate = date.toLocaleDateString('vi-VN', {
+                            day: '2-digit',
+                            month: '2-digit',
+                            year: 'numeric'
+                        });
+                        return gridjs.html(`<a >${formattedDate}</a>`);
+                    }
+                }, {
+                    name: "Trạng thái",
+                    width: "auto",
+                    formatter: function (lien, row) {
+                        let trangThaiViet = {
+                            'chua_ho_tro' : 'Chưa hỗ trợ',
+                            'duyet' : 'Đã duyệt',
+                            'tu_choi' : 'Đã từ chối'
+                        };
+
+                        let statusClass = '';
+                        switch (lien) {
+                            case 'chua_ho_tro':
+                                statusClass = 'status-chua_ho_tro';
+                                break;
+                            case 'duyet':
+                                statusClass = 'status-duyet';
+                                break;
+                            case 'tu_choi':
+                                statusClass = 'status-tu_choi';
+                                break;
+                        }
+
+                        return gridjs.html(`
+                                <div class="btn-group btn-group-sm" id="status-${row.cells[0].data}"
+                                    onmouseover="showStatusOptions(${row.cells[0].data})"
+                                    onmouseout="hideStatusOptions(${row.cells[0].data})">
+
+                                    <button type="button" class="btn ${statusClass}">${trangThaiViet[lien]}</button>
+                                    <button type="button" class="btn ${statusClass} dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <span class="visually-hidden">Toggle Dropdown</span>
+                                    </button>
+                                   <ul class="dropdown-menu" id="status-options-${row.cells[0].data}">
+                                        <li><a class="dropdown-item" href="#" onclick="changeStatus(${row.cells[0].data}, 'tu_choi')">Đã từ chối</a></li>
+                                        <li><a class="dropdown-item" href="#" onclick="changeStatus(${row.cells[0].data}, 'duyet')">Đã duyệt</a></li>
+                                        <li><a class="dropdown-item" href="#" onclick="changeStatus(${row.cells[0].data}, 'chua_ho_tro')">Chưa hỗ trợ</a></li>
+                                   </ul>
+                                </div>
+                            `);
+                    }
+                },
+            ],
+            pagination: {
+                limit: 5
+            },
+            sort: !0,
+            search: !0,
+            data: [
+                    @foreach ($congTacViens as $yeucau)
+                [
+                    '{{ $yeucau->id }}',
+                    '{{ $yeucau->ten_doc_gia }}',
+                    '{{ $yeucau->user->email }}',
+                    '{{ $yeucau->so_dien_thoai }}',
+                    '{{ $yeucau->sinh_nhat }}',
+                    '{{ $yeucau->trang_thai }}',
+                ],
+                @endforeach
+            ]
+        }).render(document.getElementById("table-gridjs"));
+
+        function showStatusOptions(id) {
+            document.getElementById('status-options-' + id).classList.remove('d-none');
+        }
+
+        // Sử lý trỏ chuột
+        function hideStatusOptions(id) {
+            document.getElementById('status-options-' + id).classList.add('d-none');
+        }
+
+        // Sử lý chuyển đổi trạng thái
+        function changeStatus(id, newStatus) {
+            if (!confirm('Bạn muốn thay đổi trạng thái rút tiền chứ?')) {
+                return;
+            }
+
+            fetch(`/admin/rut-tien/${id}/update-status`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+                },
+                body: JSON.stringify({status: newStatus})
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success) {
+                        let trangThaiViet = {
+                            'da_huy' : 'Đã hủy',
+                            'da_duyet' : 'Đã duyệt',
+                            'dang_xu_ly' : 'Đang xử lý'
+                        };
+
+                        let statusClass = '';
+                        switch (newStatus) {
+                            case 'da_huy':
+                                statusClass = 'status-da_huy';
+                                break;
+                            case 'da_duyet':
+                                statusClass = 'status-da_duyet';
+                                break;
+                            case 'dang_xu_ly':
+                                statusClass = 'status-dang_xu_ly';
+                                break;
+                        }
+
+                        // Cập nhật nút trạng thái và màu sắc dropdown
+                        let statusButton = document.querySelector(`#status-${id} .btn`);
+                        let dropdownToggle = document.querySelector(`#status-${id} .dropdown-toggle`);
+
+                        statusButton.className = `btn ${statusClass}`;
+                        statusButton.textContent = trangThaiViet[newStatus];
+
+                        // Cập nhật màu sắc của mũi tên
+                        dropdownToggle.className = `btn ${statusClass} dropdown-toggle dropdown-toggle-split`;
+                        dropdownToggle.style.borderTopColor = statusButton.style.color;
+
+
+                        hideStatusOptions(id);
+
+                        // // Cập nhật số dư mới
+                        // if (data.new_balance) {
+                        //     const soDuElement = document.getElementById('soDu');
+                        //     console.log(soDuElement); // Xem giá trị của phần tử
+                        //     if (soDuElement) {
+                        //         soDuElement.textContent = data.new_balance;
+                        //     } else {
+                        //         console.error('Phần tử với ID "soDu" không tồn tại.');
+                        //     }
+                        // }
+
+
+                    } else {
+                        alert(data.message || 'Không thể cập nhật trạng thái này.');
+                    }
+
+
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    alert('Có lỗi xảy ra. Vui lòng thử lại.');
+                });
+
+        }
+
+
+    </script>
+
+    <style>
+        /* Màu của nút */
+        .status-dang_xu_ly {
+            background-color: #ffa500;
+            color: #fff;
+        }
+
+        .status-da_duyet {
+            background-color: green;
+            color: #fff;
+        }
+
+        .status-da_huy {
+            background-color: red;
+            color: #fff;
+        }
+
+        .status-dang_xu_ly:hover {
+            background-color: #ffa500;
+            color: #fff;
+        }
+
+        .status-da_duyet:hover {
+            background-color: green;
+            color: #fff;
+        }
+
+        .status-da_huy:hover {
+            background-color: red;
+            color: #fff;
+        }
+
+        /*.status-da_huy .dropdown-menu {*/
+        /*    background-color: red;*/
+        /*}*/
+
+        .btn-group-sm .btn {
+            font-size: 0.75rem;
+            padding: 0.25rem 0.5rem;
+            height: 1.5rem;
+        }
+
+        .dropdown-menu {
+            font-size: 0.75rem;
+        }
+
+        .btn-group-sm .dropdown-menu {
+            min-width: 80px;
+        }
+
+    </style>
+@endpush
