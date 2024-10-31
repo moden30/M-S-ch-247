06336ef23e99c7a366032b048b2bdfcf62967a26
giapdn <donguyengiapicloud@gmail.com>
2024-11-01 00:16:51 +0700

    maintain giao diện

--- app/Http/Middleware/ShareCommonData.php
+++ app/Http/Middleware/ShareCommonData.php
@@ -6,7 +6,10 @@ use App\Models\BaiViet;
 use App\Models\Banner;
 use App\Models\ChuyenMuc;
 use App\Models\TheLoai;
+use App\Models\ThongBao;
+use App\Models\YeuThich;
 use Closure;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\View;
 
 class ShareCommonData
@@ -14,12 +17,24 @@ class ShareCommonData
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
+        $countThongBaos = 0;
+        $countYeuThichs = 0;
+
+        if (Auth::check()) {
+            $userId = auth()->id();  // Lấy id của user đăng nhập một lần để sử dụng nhiều lần
+            $countThongBaos = ThongBao::where('trang_thai', 'chua_xem')
+                ->where('user_id', $userId)
+                ->count();
+            $countYeuThichs = YeuThich::where('user_id', $userId)
+                ->count();
+        }
+
         $commonData = [
             'theLoais' => TheLoai::where('trang_thai', 'hien')->get(),
             'baiviet' => BaiViet::where('trang_thai', 'hien')->get(),
@@ -29,10 +44,11 @@ class ShareCommonData
                         $query->where('trang_thai', 'hien');
                     }]);
             }])
-            ->whereNull('chuyen_muc_cha_id')
-            ->where('trang_thai', 'hien')
-            ->get(),
-
+                ->whereNull('chuyen_muc_cha_id')
+                ->where('trang_thai', 'hien')
+                ->get(),
+            'countThongBaos' => $countThongBaos,
+            'countYeuThichs' => $countYeuThichs,
             // 'slideImg' => Banner::with('hinhAnhBanner '),
         ];
 
