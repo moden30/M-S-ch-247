ff6765bb5f4fd42b43fcc09df0a55ca32ef3c6b2
giapdn <donguyengiapicloud@gmail.com>
2024-11-23 01:50:13 +0700

    tạo Job async khi cần gửi mail thông báo cho admin có yêu cầu rút tiền mới

--- app/Jobs/NewCashoutReqestEmail.php
+++ app/Jobs/NewCashoutReqestEmail.php
@@ -7,24 +7,41 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
 
 class NewCashoutReqestEmail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    protected $adminUser;
+    protected $userName;
+    protected $soTien;
+    protected $url;
+
     /**
      * Create a new job instance.
      */
-    public function __construct()
+    public function __construct($adminUser, $userName, $soTien, $url)
     {
-        //
+        $this->adminUser = $adminUser;
+        $this->userName = $userName;
+        $this->soTien = $soTien;
+        $this->url = $url;
     }
 
     /**
      * Execute the job.
      */
-    public function handle(): void
+    public function handle()
     {
-        //
+        Mail::raw(
+            'Có yêu cầu rút tiền mới từ tài khoản "' . $this->userName .
+            '" với số tiền ' . number_format($this->soTien, 0, ',', '.') .
+            ' VNĐ. Bạn có thể xem yêu cầu tại đây: ' . $this->url,
+            function ($message) {
+                $message->to($this->adminUser->email)
+                    ->subject('Thông báo yêu cầu rút tiền mới');
+            }
+        );
     }
 }
